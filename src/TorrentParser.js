d2d2cc07a19503b3ad3925fc55761fa20621df81
Dachel Akyerefi <dachelakyerefi@Dachels-MacBook-Pro.local>
2025-05-19 20:34:10 +0000

    Included announce request and response

--- src/TorrentParser.js
+++ src/TorrentParser.js
@@ -1,11 +1,20 @@
 import fs from 'fs';
 import bencode from 'bencode';
+import BigNum from 'bignum';
 
 const open = (filePath) => {
     return bencode.decode(fs.readFileSync(filePath));
 } 
-const size = (torrent) => {}
-const getInfoHash = (torrent) => {} 
+const size = (torrent) => {
+    const size = torrent.info.files ?
+        torrent.info.files.map(file => file.length).reduce((a,b) => a + b) : torrent.info.length;
+
+        return BigNum.toBuffer(size, { size: 8 });
+}
+const getInfoHash = (torrent) => {
+    const info = bencode.encode(torrent.info);
+    return crypto.createHash('sha1').update(info).digest();
+} 
 
 export default {
     open,
