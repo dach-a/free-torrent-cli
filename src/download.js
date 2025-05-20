744b76c0d97a1ae442e588ddc61de022c5a1a1c4
Dachel Akyerefi <dachelakyerefi@Dachels-MacBook-Pro.local>
2025-05-20 16:18:02 +0000

    Download module expanded to handle TCP Handshakes with message handler function

--- src/download.js
+++ src/download.js
@@ -1,21 +1,23 @@
 import net from 'net';
 import { Buffer } from 'buffer';
 import { getPeers } from './tracker.js';
+import { buildHandshake, buildInterested } from './Message.js';
 
 export default (torrent) => {
     getPeers(torrent, peers => {
-        peers.forEach(download)
+        // peers is an array of objects with ip and port properties
+        peers.forEach(peer => download(peer, torrent));
     })
 }
-const download = (peer) => {
+const download = (peer, torrent) => {
     const socket = net.Socket();
     socket.on('error', console.log)
     socket.connect(peer.port, peer.ip, () => {
-
+        // send handshake
+        socket.write(buildHandshake(torrent));
     })
-    onWholeMsg(socket, data => {
-
-    });
+    // listen for messages
+    onWholeMsg(socket, msg => msgHandler(msg, socket));
     socket.on('data', data => {
 
     })
@@ -35,4 +37,11 @@ const onWholeMsg = (socket, callback) => {
             handshake = false;
         }
     });
+}
+const msgHandler = (msg, socket) => {
+    if(isHandshake(msg)) socket.write(buildInterested());
+}
+const isHandshake = msg => {
+    return msg.length === msg.readUInt8(0) + 49 &&
+        msg.toString('utf8', 1) === 'BitTorrent protocol';
 }
